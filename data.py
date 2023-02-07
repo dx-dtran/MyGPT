bd971d9bc2600251ff80f602d1482578bff309af
daniel <dxdtran@outlook.com>
2023-02-06 22:12:03 -0800

    add new dataset and outputs

---
+++ data.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+with open('math.txt', 'w') as write_output:
+    for subdir, dirs, files in os.walk('./MATH/train'):
+        for file in files:
+            # print os.path.join(subdir, file)
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".json"):
+                # print(filepath)
+                with open(filepath, 'r') as f:
+                    data = json.load(f)
+                    write_output.writelines(data['solution'])
+
