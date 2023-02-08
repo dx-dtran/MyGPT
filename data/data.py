2c941e48b66edd1e3c3b38cf7978ac3a58fdff54
daniel <dxdtran@outlook.com>
2023-02-07 22:23:30 -0800

    move data files

--- data/data.py
+++ data/data.py
@@ -2,7 +2,7 @@ import json
 import os
 
 with open('math.txt', 'w') as write_output:
-    for subdir, dirs, files in os.walk('./MATH/train'):
+    for subdir, dirs, files in os.walk('../MATH/train'):
         for file in files:
             # print os.path.join(subdir, file)
             filepath = subdir + os.sep + file
